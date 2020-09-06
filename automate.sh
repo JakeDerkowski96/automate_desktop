97bc323c498bb6b16ac9f7c20222c62282a2d2a9
JakeDerkowski96 <jakederkowski12@gmail.com>
2020-09-06 03:04:31 -0500

    final

--- automate.sh
+++ automate.sh
@@ -11,43 +11,39 @@ FUNC=".functions.sh"
 source $FUNC
 
 # automate installaton
-auto_install(){
-  read -p "Automate installation? [Y/n]?  : " answer
-  while true
-  do
-    case $answer in
-      [Yy]* ) echo -e "ok, i got ya"; echo; sleep 0.5;
+read -p "Automate installation? [Y/n]?  : " answer
+while true
+do
+  case $answer in
+    [Yy]* ) echo -e "ok, i got ya"; echo; sleep 0.5;
 
-      echo -e "Details found in the 'logs' folder";
-      check_dir logs;
-      echo "Installing prerequistes"; sleep 0.5;
-      required > "logs/prereqs.txt";
-      check_dir scripts;
+    echo -e "Details found in the 'logs' folder";
+    check_dir logs;
+    echo "Installing prerequistes"; sleep 0.5;
+    required > "logs/prereqs.txt";
+    check_dir scripts;
 
-      # note
-      echo "automate other deb packages coming soon.."; sleep 1;
+    # note
+    echo "automate other deb packages coming soon.."; sleep 1;
 
-      echo -e "beginning..."; sleep 0.5;
+    echo -e "beginning..."; sleep 0.5;
 
-      # dispaly pretty terminal
-      show_host_ip;
+    # dispaly pretty terminal
+    show_host_ip;
 
-      # check_dir "deb";
-      pprint Terminal;
-      term_check;
+    # check_dir "deb";
+    pprint Terminal;
+    term_check;
 
-      get_ans "packages" $PKGS;
-      get_ans "snaps" $SNAPS;
+    get_ans "packages" $PKGS;
+    get_ans "snaps" $SNAPS;
 
-      break;;
+    break;;
 
-      [Nn]* ) echo -e "why run this then?"
-              exit;;
+    [Nn]* ) echo -e "why run this then?"
+            exit;;
 
-      * ) echo "Invalid input";
-          break;;
-    esac
-  done
-}
-
-auto_install;
+    * ) echo "Invalid input";
+        break;;
+  esac
+done
