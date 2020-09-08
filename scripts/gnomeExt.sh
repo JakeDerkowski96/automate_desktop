cfe239fc0467a33dedb034e112e880812a9ce1f9
Jake Derkowski <derkowskijake@live.com>
2020-09-07 23:22:42 -0500

    Create gnomeExt.sh

---
+++ scripts/gnomeExt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# a record of the gnome shell extensions that I use 
+# implementation of these will be done some day
+
+echo -e "my shell extensions"
+ 
+ 
+ EXTENSTION="http://git.gnome.org/gnome-shell-extensions
+ https://gitlab.gnome.org/GNOME/gnome-shell-extensions
+ http://github.com/mpdeimos/gnome-shell-remove-dropdown-arrows
+ https://gitlab.gnome.org/GNOME/gnome-shell-extensions
+ https://gitlab.com/eccheng/customcorner"
+ 
+ echo " no point to run this script
+  but if you do you will get a list of these to a file"
+  
+  echo $EXTENSIONS > jdGnomeExtensions
+  
+  exit 0
